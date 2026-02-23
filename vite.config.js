f2395a97ea467f561b7778ab88061ecef8dba869
YOzozay <Pongpan1311@gmail.com>
2026-02-23 11:22:30 +0700

    update path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/MyHub-Finance/', // เพิ่มบรรทัดนี้ (ใส่ชื่อ Repository ของคุณระหว่างเครื่องหมาย / /)
+  base: '/p-z8-p2-w4-finance-mngr/', // เพิ่มบรรทัดนี้ (ใส่ชื่อ Repository ของคุณระหว่างเครื่องหมาย / /)
 })
