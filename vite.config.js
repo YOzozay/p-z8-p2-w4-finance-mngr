5a7477e86cd269412dc89187ff0d60016bc84185
YOzozay <Pongpan1311@gmail.com>
2026-02-23 11:01:57 +0700

    update config for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/MyHub-Finance/', // เพิ่มบรรทัดนี้ (ใส่ชื่อ Repository ของคุณระหว่างเครื่องหมาย / /)
 })
